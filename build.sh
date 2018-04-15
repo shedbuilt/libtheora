11e5be599d3833e278afc53acf8ffb67e9d4c8e8
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:21:21 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ sed -i 's/png_\(sizeof\)/\1/g' examples/png2theora.c && \
 ./configure --prefix=/usr \
             --build="$SHED_NATIVE_TARGET" \
             --disable-static && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
